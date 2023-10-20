fe470bf7a43f0bdcbffa10ebcf58bd69655bc11d
Terry <antonypaco12@gmail.com>
2023-10-19 22:40:33 -0500

    v5 Compo-P07

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@
 
 import AppForm from './components/AppForm';
 import C01componente from './components/C01componente';
+import MatrizOperaciones from './components/P7matrizOperaciones';
 
 
 function App() {
   return (
-    <div style={{background:"yellow",
+    <div style={{background:"green",
     width:"350px",
     padding:"10px"}}>
       <AppForm/>
       <i class="large material-icons">account_box</i>
-
+      <MatrizOperaciones/>
     </div>
   );
 }
